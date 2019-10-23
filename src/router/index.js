0a1eec3ab87f36d6fb7dd762daab13734f3e121a
Peter Kuehfuss <peter.kuehfuss@kmpc.de>
2019-10-23 16:48:17 +0200

    Init authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from '@/components/Login'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   },
   {
     path: '/about',
