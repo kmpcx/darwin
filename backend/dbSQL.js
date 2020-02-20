2d1f095759321771980ffc7f44f3e4ae60a33965
Peter Kuehfuss <peter.kuehfuss@kmpc.de>
2020-02-20 14:57:36 +0100

    Add second DB

--- backend/dbSQL.js
+++ backend/dbSQL.js
@@ -9,8 +9,28 @@ const pool = mysql.createPool({
   debug:  false
 });
 
+const poolSage = mysql.createPool({
+  connectionLimit : 20,
+  host: '***REMOVED***',
+  user: '***REMOVED***',
+  password: '***REMOVED***',
+  database: '***REMOVED***',
+  debug:  false
+});
+
 function handle_db(query, callback){
   pool.query(query, function(err, data){
+    if(err) {
+      console.log(err);
+      return callback({'error': true, 'data': 'Error occurred - ' + err});
+    } else {
+      return callback({'error': false, 'data': data});
+    }
+   });
+};
+
+function handle_db_sageDB(query, callback){
+  poolSage.query(query, function(err, data){
     if(err) {
       console.log(err);
       return callback({'error': true, 'data': 'Error occurred - ' + err});
