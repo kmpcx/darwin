54b7530d8cd113645c91dee54ff56696fad2950c
Martin Kinitzki <martin.kinitzki@kmpc.de>
2020-03-25 13:51:35 +0100

    Login validation and alert messages

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -8,31 +8,40 @@
     <br />
     <v-card tile>
       <v-card-title class="headline">Login</v-card-title>
-        <v-row>
-          <v-col cols="12">
-            <v-form @submit.prevent="login">
-              <v-container>
-                <v-row>
-                  <v-col cols="12" md="5">
-                    <v-text-field v-model="username" label="Benutzername" required type="text"></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="4">
-                    <v-text-field
-                      v-model="password"
-                      label="Passwort"
-                      required
-                      type="password"
-                      placeholder="Passwort"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" md="3">
-                    <button type="submit">Login</button>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-form>
-          </v-col>
-        </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-form @submit.prevent="login">
+            <v-container>
+              <v-row>
+                <v-col cols="12" md="5">
+                  <v-text-field
+                    v-model="username"
+                    label="Benutzername"
+                    required
+                    type="text"
+                    :rules="nameRules"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="4">
+                  <v-text-field
+                    v-model="password"
+                    label="Passwort"
+                    required
+                    type="password"
+                    :rules="pwRules"
+                  ></v-text-field>
+                </v-col>
+                <v-col cols="12" md="3">
+                  <button type="submit">Login</button>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-form>
+        </v-col>
+      </v-row>
+      <v-snackbar v-model="alert" top right color="error" multi-line timeout="3000">
+        {{ alertText }}
+      </v-snackbar>
     </v-card>
     <br />
   </div>
@@ -40,20 +49,45 @@
 
 <script>
 export default {
-  data() {
-    return {
-      username: "",
-      password: ""
-    };
-  },
+  // data() {
+  //   return {
+  //     username: "",
+  //     password: ""
+  //   };
+  // },
+  data: () => ({
+    return: { username: "", password: "" },
+    alert: false,
+    alertText: "Zugriff verweigert. Bitte überprüfen Sie Ihre Zugangsdaten.",
+    valid: true,
+    username: "",
+    password: "",
+    nameRules: [v => !!v || "Benutzername ist ein Pflichtfeld"],
+    pwRules: [v => !!v || "Passwort ist ein Pflichtfeld"]
+  }),
+
   methods: {
+    openAlert: function(textAlert) {
+      this.alert = true;
+      this.alertText = textAlert;
+    },
+    validate() {
+      this.$refs.form.validate();
+    },
     login: function() {
       let username = this.username;
       let password = this.password;
+      let self = this;
       this.$store
         .dispatch("login", { username, password })
         .then(() => this.$router.push("/"))
-        .catch(err => console.log(err));
+        .catch(function(error) {
+          if (error.response && error.response.status === 404) {
+            self.openAlert("Benutzername falsch oder Benutzer nicht im System.");
+          } else if (error.response && error.response.status === 401) {
+            self.openAlert("Passwort für den Benutzer nicht korrekt.");
+          }
+        });
     }
   }
 };
